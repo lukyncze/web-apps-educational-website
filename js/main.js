cf0f8d6fd649256538fa296a96b146d76b15ec27
lukyncze <lukas.lukyn.sukenik@gmail.com>
2021-03-31 10:11:14 +0200

    0.3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-sal({threshold: 0.01,});
+sal({ threshold: 0.01, });
+
+$(function () {
+    // SCROLLING TO TOP OF THE PAGE
+    if ($(window).scrollTop() > ($(window).height() / 2)) {
+        $('#scrollToTop').show();
+    } else {
+        $('#scrollToTop').hide();
+    }
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > ($(window).height() / 2)) {
+            $('#scrollToTop').fadeIn(100);
+        } else {
+            $('#scrollToTop').fadeOut(100);
+        }
+    });
+
+    $("#scrollToTop").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 50);
+    });
+
+    // PRELOADER
+    $(document).ready(function(){
+        $(".loader-container").fadeOut(1000);
+    });
+});
